8cb5216ed4676acb2648b4611b52cfea0bcb0bd7
Nadir2087 <95905993+Nadir2087@users.noreply.github.com>
2023-01-16 17:45:14 +0600

    this commit for Header and start work on cotalog and cotalog-item

---
+++ src/components/v-cotalog.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name:'v-cotalog'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/v-header.vue
@@ -0,0 +1,51 @@
+<template>
+  <header class="v-header">
+    <div class="container">
+        <div class="header_wraper">
+            <div class="header_logo">logo</div>
+                <nav class="header_nav">
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">link1</a></li>
+                        <li><a href="">link2</a></li>
+                    </ul>
+            </nav>
+        </div>
+    </div> 
+  </header>
+</template>
+
+<script>
+export default {
+    name:'v-header',
+    components:{},
+}
+</script>
+
+<style lang="scss">
+    .v-header{
+        .header_wraper{
+            border-bottom: 1px solid #c6c6c6;
+            padding: 0 10px;
+            display: flex;
+            align-items: center;
+            justify-content:space-between;
+            .header_logo{
+                font-size: 30px;
+                font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+                font-weight: bold;
+            }
+            .header_nav ul{
+                list-style-type: none;
+                display: flex;
+                li{
+                    margin-left: 10px;
+                }
+            }
+        }
+        .container{
+            max-width: 1100px;
+            margin: 0 auto;
+        }
+    }
+</style>
